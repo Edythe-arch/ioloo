4ca8acf874bb94d87a436b454146b5ac1bdec9e4
Edythe-arch <edytheboone@hotmail.com>
2026-02-04 16:52:38 +0530

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+# Configuration file for the Sphinx documentation builder
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'IOLOO'
+copyright = '2025'
+author = 'Carolyn Castano'
+release = '1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
